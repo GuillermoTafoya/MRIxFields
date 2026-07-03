8589bc19cfcdccfc8cab1fee2e69e88080a6ca26
Guillermo Tafoya <tafoyamilo@gmail.com>
2026-07-02 22:20:33 -0600

    feat(models): add conditional U-Net field translator

--- src/fieldbridge/models/__init__.py
+++ src/fieldbridge/models/__init__.py
@@ -6,6 +6,7 @@ from fieldbridge.models.conditioning import DomainConditioner, DomainEmbedding
 from fieldbridge.models.factory import build_decoder, build_encoder, build_translator
 from fieldbridge.models.film import FiLMGroupNorm, FiLMLayer
 from fieldbridge.models.translators.conditional_cnn import ConditionalCNNFieldTranslator
+from fieldbridge.models.translators.conditional_unet import ConditionalUNetFieldTranslator
 from fieldbridge.models.translators.identity import IdentityTranslator
 
 __all__ = [
@@ -16,6 +17,7 @@ __all__ = [
     "CNNDecoder",
     "CNNEncoder",
     "ConditionalCNNFieldTranslator",
+    "ConditionalUNetFieldTranslator",
     "IdentityDecoder",
     "IdentityEncoder",
     "IdentityTranslator",
--- src/fieldbridge/models/factory.py
+++ src/fieldbridge/models/factory.py
@@ -15,6 +15,7 @@ from fieldbridge.models.autoencoders.cnn_autoencoder import CNNDecoder, CNNEncod
 from fieldbridge.models.autoencoders.identity import IdentityDecoder, IdentityEncoder
 from fieldbridge.models.translators.base import BaseTranslator
 from fieldbridge.models.translators.conditional_cnn import ConditionalCNNFieldTranslator
+from fieldbridge.models.translators.conditional_unet import ConditionalUNetFieldTranslator
 from fieldbridge.models.translators.identity import IdentityTranslator
 
 T = TypeVar("T")
@@ -32,6 +33,8 @@ _TRANSLATORS: dict[str, Callable[..., BaseTranslator]] = {
     "cnn_autoencoder": IdentityTranslator,
     "conditional_cnn": ConditionalCNNFieldTranslator,
     "conditional_cnn_field_translator": ConditionalCNNFieldTranslator,
+    "conditional_unet": ConditionalUNetFieldTranslator,
+    "conditional_unet_field_translator": ConditionalUNetFieldTranslator,
 }
 
 
